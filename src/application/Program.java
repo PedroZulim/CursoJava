90df7bf1d6d32fc641271107ae472b0306f7ec59
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2025-09-10 00:35:43 -0300

    soma_vetor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,34 @@
 package application;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
-		
-		int[] vet = new int[n];
-		
-		for(int i = 0; i < vet.length; i++) {
-			vet[i] = sc.nextInt();		
+
+		double[] vet = new double[n];
+
+		double soma = 0;
+
+		for (int i = 0; i < vet.length; i++) {
+			vet[i] = sc.nextDouble();
 		}
-		
-		for(int i = 0; i < vet.length; i++) {
-			if(vet[i]<0) {
-				System.out.println(vet[i]);
-			}
+
+		System.out.print("Valores: ");
+		for (int i = 0; i < vet.length; i++) {
+			System.out.print(vet[i] + " ");
+			soma += vet[i];
+
 		}
-		
+		System.out.println();
+		System.out.println("SOMA = " + soma);
+		System.out.println("MEDIA = " + soma / vet.length);
+
 		sc.close();
 	}
 
