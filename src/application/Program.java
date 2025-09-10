a946a7bf665d6b119d203979ec2d5068c1098562
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2025-09-10 01:05:52 -0300

    vetor with entities

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,31 +3,46 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Peoples;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
+		System.out.print("Quantas pessoas serao digitadas? ");
 		int n = sc.nextInt();
 
-		double[] vet = new double[n];
+		Peoples[] vet = new Peoples[n];
 
 		double soma = 0;
+		double soma2 = 0;
 
 		for (int i = 0; i < vet.length; i++) {
-			vet[i] = sc.nextDouble();
+			System.out.println("Dados da " + (i+1) + "a pessoa: ");
+			sc.nextLine();
+			String nome = sc.nextLine();
+			int idade = sc.nextInt();
+			double altura = sc.nextDouble();
+			vet[i] = new Peoples(nome, idade, altura);
 		}
 
-		System.out.print("Valores: ");
 		for (int i = 0; i < vet.length; i++) {
-			System.out.print(vet[i] + " ");
-			soma += vet[i];
-
+			soma+=vet[i].getAltura();
+			if(vet[i].getIdade()<16) {
+				soma2++;
+			}
 		}
 		System.out.println();
-		System.out.println("SOMA = " + soma);
-		System.out.println("MEDIA = " + soma / vet.length);
+		System.out.println("Altura média: " + soma / vet.length);
+		System.out.printf("Pessoas com menos de 16 anos: %.1f%n", (soma2 / vet.length) * 100.0);
+		
+		for (int i = 0; i < vet.length; i++) {
+			if(vet[i].getIdade()<16) {
+				System.out.println(vet[i].getNome());
+			}
+		}
 
 		sc.close();
 	}
