31eeb35b394a1282c7355915a026c8e028cbb829
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2025-09-10 00:25:30 -0300

    simple vetor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,27 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[] vet = new int[n];
+		
+		for(int i = 0; i < vet.length; i++) {
+			vet[i] = sc.nextInt();		
+		}
+		
+		for(int i = 0; i < vet.length; i++) {
+			if(vet[i]<0) {
+				System.out.println(vet[i]);
+			}
+		}
+		
+		sc.close();
 	}
 
 }
